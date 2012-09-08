8cb596d1c7bd3491acfdf6fb1e2e696d22ca0cc8
han <hanaldo520124@hotmail.com>
2012-09-08 18:03:25 -0400

    add UML for a2.

--- a2/mjaffee-sjeganat-vrajasek-shenhan/shenhan/p532/a2/actions/ProceedGameFrame.java
+++ a2/mjaffee-sjeganat-vrajasek-shenhan/shenhan/p532/a2/actions/ProceedGameFrame.java
@@ -1,18 +1,14 @@
 package shenhan.p532.a2.actions;
 
-import shenhan.p532.a2.model.GameObject;
-import shenhan.p532.a2.model.Ball;
-import shenhan.p532.a2.model.Brick;
-import shenhan.p532.a2.model.Paddle;
-import shenhan.p532.a2.model.BonusBall;
-import acm.graphics.GObject;
 import acm.graphics.GPoint;
 import acm.util.MediaTools;
 import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import org.apache.log4j.Logger;
 import shenhan.p532.a2.controller.GameController;
+import shenhan.p532.a2.model.*;
 import shenhan.p532.a2.shapes.MyShape;
 
 /**
@@ -20,26 +16,27 @@ import shenhan.p532.a2.shapes.MyShape;
  * @author han
  */
 public class ProceedGameFrame implements ActionListener {
-
+    
+    private static final Logger log = Logger.getLogger(ProceedGameFrame.class.getName());
     private List<GameObject> gameObjects;
     private GameController gameController;
     private AudioClip bounceSound;
     private AudioClip bonusSound;
-
+    
     public ProceedGameFrame(GameController gameController) {
         this.gameObjects = gameController.getGameObjects();
         this.gameController = gameController;
-
+        
         try {
             bounceSound = MediaTools.loadAudioClip("bounce.au");
             bonusSound = MediaTools.loadAudioClip("fire.au");
         } catch (Exception e) {
             bounceSound = null;
             bonusSound = null;
-            System.err.println(e.toString());
+            log.warn(e);
         }
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent ae) {
         for (GameObject gameObject : gameObjects) {
@@ -48,13 +45,13 @@ public class ProceedGameFrame implements ActionListener {
         }
         checkCollision();
     }
-
+    
     private void checkCollision() {
         checkWallCollision();
         checkBallCollision();
         checkBonusCollision();
     }
-
+    
     private void checkWallCollision() {
         boolean[] hitWall = detectCollidingWall(GameController.GAME_AREA_TOPLEFT_X, GameController.GAME_AREA_TOPLEFT_Y, GameController.GAME_AREA_RIGHTBOTTOM_X, GameController.GAME_AREA_RIGHTBOTTOM_Y);
         if (hitWall[1]) {
@@ -70,27 +67,24 @@ public class ProceedGameFrame implements ActionListener {
             playBounceSound();
         }
     }
-
+    
     private void checkBallCollision() {
         MyShape myShape = gameController.getGameBall().getCollidingGameShape();
-
+        
         if (myShape == null) {
             return;
         }
-
-        //System.out.println(myShape);
-
+        
         if (myShape.getOwner() instanceof Brick) {
             Brick brick = (Brick) myShape.getOwner();
-
+            
             Ball gameBall = gameController.getGameBall();
-
+            
             brick.setVisible(false);
-            //gameController.remove((GObject) myShape);
-
+            
             gameBall.setDxDy(gameBall.getDxDy()[0], -gameBall.getDxDy()[1]);
             gameController.setBrickNumber(gameController.getBrickNumber() - 1);
-
+            
             if (gameController.getBrickNumber() == 0) {
                 gameController.pauseGame();
                 gameController.displayMessage("Yon Won! Click Start to play again!");
@@ -104,61 +98,61 @@ public class ProceedGameFrame implements ActionListener {
                     brick.getBonusBall().setDxDy(0, 2);
                 }
             }
-
+            
             playBounceSound();
         } else if (myShape.getOwner() instanceof Paddle) {
             Ball gameBall = gameController.getGameBall();
-
+            
             GPoint difBetween = differenceBetweenPoints(gameBall.getPosition(), myShape.getOwner().getPosition());
-
+            
             double newVX = (difBetween.getX() - (gameBall.getBounds()[3] / 2)) / 2;
             double newVY = -(difBetween.getY() / gameBall.getDxDy()[1]);
-
+            
             if (newVY == 0.0) {
                 newVY = 1.0;
             }
-
+            
             newVY = -newVY;
             gameBall.setDxDy(newVX, newVY);
-
+            
             playBounceSound();
         }
     }
-
+    
     private void checkBonusCollision() {
         for (BonusBall bonus : gameController.getBonusBalls()) {
             if (!bonus.isVisible()) {
                 continue;
             }
-
+            
             MyShape myShape = bonus.getCollidingGameShape();
-
+            
             if (myShape == null) {
                 return;
             }
             
-            System.out.println(bonus);
-
+            log.debug(bonus.toString());
+            
             if (myShape.getOwner() instanceof Paddle) {
-                System.out.println(myShape + " " + myShape.getOwner());
+                log.debug(myShape + " " + myShape.getOwner());
                 
                 bonus.setVisible(false);
                 int[] bounds = myShape.getOwner().getBounds();
                 myShape.getOwner().setBounds(bounds[0], bounds[1], bounds[2] + 40, bounds[3]);
                 playBonusSound();
             }
-
+            
             
         }
     }
-
+    
     private GPoint differenceBetweenPoints(int[] p1, int[] p2) {
         return new GPoint(p1[0] - p2[0], p1[1] - p2[1]);
     }
-
+    
     private boolean[] detectCollidingWall(int leftTopX, int leftTopY, int rightBottomX, int rightBottomY) {
         Ball gameBall = gameController.getGameBall();
-
+        
         boolean hitWall = false;
         boolean gameEnd = false;
         if (gameBall.getPosition()[0] >= rightBottomX) {
@@ -174,13 +168,13 @@ public class ProceedGameFrame implements ActionListener {
             hitWall = true;
             gameEnd = true;
         }
-
+        
         return new boolean[]{hitWall, gameEnd};
     }
-
+    
     private void playBounceSound() {
         new Thread() {
-
+            
             @Override
             public void run() {
                 if (bounceSound != null) {
@@ -189,10 +183,10 @@ public class ProceedGameFrame implements ActionListener {
             }
         }.start();
     }
-
+    
     private void playBonusSound() {
         new Thread() {
-
+            
             @Override
             public void run() {
                 if (bonusSound != null) {
