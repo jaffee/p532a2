d509a788dbc83bdd308324de60934ad185e9aebc
harsh savla <hsavla@indiana.edu>
2012-09-09 14:20:51 -0400

    added comments

--- a2/cpanchag-hsavla-mmasrani-ritekavu/breakout_assgn_2_v_1_0/CommandClass.java
+++ a2/cpanchag-hsavla-mmasrani-ritekavu/breakout_assgn_2_v_1_0/CommandClass.java
@@ -1,5 +1,7 @@
 package breakout_assgn_2_v_1_0;
 
+/*This class is used to set and execute command. It acts the invoker of the command in the Command pattern */
+
 public class CommandClass {
 
 	private Command command;
--- a2/cpanchag-hsavla-mmasrani-ritekavu/breakout_assgn_2_v_1_0/ControlClass.java
+++ a2/cpanchag-hsavla-mmasrani-ritekavu/breakout_assgn_2_v_1_0/ControlClass.java
@@ -18,6 +18,8 @@ import javax.swing.Timer;
 /** This is the class where the actual logic of the game is implemented. The
  * control of the class is given to the key listener and action listener to
  * interact with the game interface.
+ *
+ * This class acts as the client in the Command pattern.
  * 
  * @param start
  *            object for the class to implement start button
@@ -43,18 +45,18 @@ public class ControlClass extends JPanel {
 	private Ball ball;
 	private Paddle paddle;
 	private ScoreBoard scorePanel;
-	private TimerHandler daemonThread;
-	private ButtonHandler actionHandler;
-	private Timer daemon;
-	private int score = 0;
+	private TimerHandler daemonThread;	// handles timer events
+	private ButtonHandler actionHandler;	// handles button events
+	private Timer daemon;			
+	private int score = 0;	// this variable is set to proper command by the function setCommand
 	private Command command;
 	private Command paddleRightMoveCommand;
 	private Command paddleLeftMoveCommand;
-	private ArrayList<SavedObjectsStates> savedObjectStates;
-	private boolean undoButtonPressedFirstTime;
-	private int whichPrevBallUndoObject;
-	private boolean isReplayChecked;
-	private CommandClass commandClass;
+	private ArrayList<SavedObjectsStates> savedObjectStates; // this arraylist stores the SavedObjectsStates which contains the state for ball, paddle and brick
+	private boolean undoButtonPressedFirstTime;	// is true when undo is pressed first time, then becomes false until start or replay is pressed
+	private int whichPrevBallUndoObject;	
+	private boolean isReplayChecked;	// this checks whether the reply button has been checked
+	private CommandClass commandClass;	// used to set and execute command
 
 	public ControlClass(ScoreBoard scorePanel, Board board, Brick bricks[], Ball ball, Paddle paddle) {
 		this.board = board;
@@ -75,12 +77,12 @@ public class ControlClass extends JPanel {
 		pause.addActionListener(actionHandler);
 		undoButton.addActionListener(actionHandler);
 		replayButton.addActionListener(actionHandler);
-		isReplayChecked = false;
+		isReplayChecked = false;					
 		savedObjectStates = new ArrayList<SavedObjectsStates>();
 		undoButtonPressedFirstTime = true;
-		paddleLeftMoveCommand = new PaddleLeftMoveCommand(paddle);
-		paddleRightMoveCommand = new PaddleRightMoveCommand(paddle);
-		commandClass = new CommandClass();
+		paddleLeftMoveCommand = new PaddleLeftMoveCommand(paddle);	// set the paddle with the paddleLeftMoveCommand
+		paddleRightMoveCommand = new PaddleRightMoveCommand(paddle);	// set the paddle with the paddleRightMoveCommand
+		commandClass = new CommandClass();				// invoker of the commands
 		this.setLayout(new GridLayout(4, 1));
 		this.add(start);
 		this.add(pause);
@@ -88,6 +90,7 @@ public class ControlClass extends JPanel {
 		this.add(replayButton);
 	}
 
+	// executed when replay button is clicked
 	public void replay() {
 		score=0;
 		scorePanel.setScore(""+score);
@@ -105,6 +108,7 @@ public class ControlClass extends JPanel {
 
 		public void actionPerformed(ActionEvent action) {
 
+			// start buton pressed
 			if (action.getActionCommand().equals("START")) {
 
 				brickCount = Constants.BRICK_COUNT;
@@ -118,8 +122,8 @@ public class ControlClass extends JPanel {
 					bricks[i].setHit(false);
 				}
 				scorePanel.setScore("0");
-				
-				
+
+				// sets the ball in random direction
 				Random r=new Random();
 				double d1=0,d2=0;
 				do 
@@ -128,6 +132,8 @@ public class ControlClass extends JPanel {
 				} while (!((6-d1*12)<3 || (6-d1*12)>3) && !((6-d2*12)<3 || (6-d2*12)>3));
 				double randomStartX=6-d1*12;
 				double randomStartY=6-d2*12;
+				
+				// initialize the ball and paddle
 				ball.setX(Constants.BALL_INTIAL_XVALUE);
 				ball.setVelocityX(randomStartX);
 				ball.setVelocityY(randomStartY);
@@ -135,22 +141,23 @@ public class ControlClass extends JPanel {
 				paddle.setX(Constants.PADDLE_INTIAL_XVALUE);
 				paddle.setY(Constants.PADDLE_INTIAL_YVALUE);
 				board.requestFocus(true);
-				commandClass.setCommand((new BallMoveCommand(ball)));
-				commandClass.buttonWasPressed();
-				board.draw();
-
-			} else if (action.getActionCommand().equals("UNDO")) {
+				commandClass.setCommand((new BallMoveCommand(ball)));	// register ballMove command
+				commandClass.buttonWasPressed();			// set the ball moving
+				board.draw();		
+			} 
+			// undo button presses
+			else if (action.getActionCommand().equals("UNDO")) {
 
 				if (undoButtonPressedFirstTime) {
 					if (savedObjectStates.size() != 0) {
 						updateTimer();
-						whichPrevBallUndoObject = savedObjectStates.size() - 1;
+						whichPrevBallUndoObject = savedObjectStates.size() - 1;	// retrieve last object
 						brickPresentChecker(whichPrevBallUndoObject);
 						savedObjectStates.get(whichPrevBallUndoObject).getBallMoveCommand().undo();
 						savedObjectStates.get(whichPrevBallUndoObject).getPaddleMoveCommand().undo();
-						undoButtonPressedFirstTime = false;
-						savedObjectStates.remove(whichPrevBallUndoObject);
-						whichPrevBallUndoObject -= 1;
+						undoButtonPressedFirstTime = false;	
+						savedObjectStates.remove(whichPrevBallUndoObject);	
+						whichPrevBallUndoObject -= 1;	// keeps track of which last ball object was retrieved
 					}
 				} else {
 
@@ -167,11 +174,13 @@ public class ControlClass extends JPanel {
 				board.draw();
 			}
 
+			// replay button pressed
 			else if (action.getActionCommand().equals("REPLAY")) {
 				if (!daemon.isRunning())
 					replay();
 			}
 
+			// pause button pressed
 			else if (action.getActionCommand().equals("PAUSE/RESUME")) {
 
 				if (daemon.isRunning()) {
@@ -182,11 +191,12 @@ public class ControlClass extends JPanel {
 
 				}
 				else
-				daemon.start();
+					daemon.start();
 				board.requestFocus(true);
 			}
 		}
 
+		// This function checks which of the bricks have been blown up and 
 		public void brickPresentChecker(int whichPrevBallUndoObject) {
 
 			boolean[] brickArray = savedObjectStates.get(whichPrevBallUndoObject).getIsBrickHit();
@@ -196,7 +206,7 @@ public class ControlClass extends JPanel {
 				if (brickArray[i] == true) {
 					if(bricks[i].isHit()!=true)
 					{
-					bricks[i].setHit(true);}
+						bricks[i].setHit(true);}
 				}
 				else if (brickArray[i] == false)
 				{
@@ -206,10 +216,10 @@ public class ControlClass extends JPanel {
 						scorePanel.setScore(""+score);
 					}
 				}
-				
 			}
 		}
 
+		// used for updating timer when undo button is pressed
 		public void updateTimer() {
 
 			timerStates = savedObjectStates.size();
@@ -227,6 +237,7 @@ public class ControlClass extends JPanel {
 
 		public void actionPerformed(ActionEvent event) {
 
+			// check if the replay button is checked
 			if (!isReplayChecked) {
 
 				clockTick++;
@@ -235,37 +246,37 @@ public class ControlClass extends JPanel {
 				scorePanel.setClock(clockTimeString);
 				checkCollision();
 
-				Command c = new BallMoveCommand(ball);
+				Command c = new BallMoveCommand(ball);		// create new ballMoveCommand instance
 				((BallMoveCommand) c).execute();
 
 				Command c1 = new PaddleMoveCommand(paddle);
 				((PaddleMoveCommand) c1).execute();
-		
-				boolean[] brickBooleanArray = new boolean[Constants.BRICK_COUNT];
+
+				boolean[] brickBooleanArray = new boolean[Constants.BRICK_COUNT];	// stores the state of the bricks
 
 				for (int i = 0; i < Constants.BRICK_COUNT; i++) {
 					brickBooleanArray[i] = bricks[i].isHit();
 				}
-				SavedObjectsStates s = new SavedObjectsStates(c, c1, brickBooleanArray);
-				savedObjectStates.add(s);
-				board.draw();
+
+				// create new instance with ballMoveCommand, paddleMoveCommand and brickBoolean array
+				SavedObjectsStates s = new SavedObjectsStates(c, c1, brickBooleanArray);	
+				savedObjectStates.add(s);		// add newly created instance to arraylist
+				board.draw();				// update the screen
 			}
 
 			else {
 				if (count < savedObjectStates.size()) {
 
 					SavedObjectsStates s = savedObjectStates.get(count);
-					Command c = s.getBallMoveCommand();
-					PaddleMoveCommand cPaddle=(PaddleMoveCommand) s.getPaddleMoveCommand();
-					boolean[] brickArray = savedObjectStates.get(count).getIsBrickHit();
+					Command c = s.getBallMoveCommand();	// retrieve previous ballMoveCommand
+					PaddleMoveCommand cPaddle=(PaddleMoveCommand) s.getPaddleMoveCommand();	// retrieve previous paddleMoveCommand
+					boolean[] brickArray = savedObjectStates.get(count).getIsBrickHit();	// retrieve previous brick array
 
 					for (int i = 0; i < Constants.BRICK_COUNT; i++) {
 
 						if (brickArray[i] == true) {
-							if(bricks[i].isHit()==true){
-								
-							}
-							else if(bricks[i].isHit()==false){
+
+							if(bricks[i].isHit()==false){
 								bricks[i].setHit(true);
 								score=score+10;
 								scorePanel.setScore(""+score);
@@ -275,10 +286,11 @@ public class ControlClass extends JPanel {
 						}
 					}
 
-					ball.setX(((BallMoveCommand) c).getPrevX());
+					ball.setX(((BallMoveCommand) c).getPrevX());	// set the ball x and y co-ordinates to previous values
 					ball.setY(((BallMoveCommand) c).getPrevY());
-					paddle.setX(cPaddle.getPrevX());
+					paddle.setX(cPaddle.getPrevX());		// set the paddle's x co-ordinate to previous value
 					count++;
+					// update the timer string
 					timerTicksReplay++;
 					double timerTickTemp = timerTicksReplay / 100.0;
 					scorePanel.setClock(((Double) timerTickTemp).toString());
@@ -301,20 +313,20 @@ public class ControlClass extends JPanel {
 		public void keyPressed(KeyEvent event) {
 
 			switch (event.getKeyCode()) {
-			case KeyEvent.VK_LEFT:
-				if (paddle.getX() > 0) {
-					commandClass.setCommand((paddleLeftMoveCommand));
-					commandClass.buttonWasPressed();
-				}
-				break;
+				case KeyEvent.VK_LEFT:
+					if (paddle.getX() > 0) {
+						commandClass.setCommand((paddleLeftMoveCommand));	// set the command to paddleLeftMove when left key is pressed
+						commandClass.buttonWasPressed();			// execute the command
+					}
+					break;
 
-			case KeyEvent.VK_RIGHT:
+				case KeyEvent.VK_RIGHT:
 
-				if (paddle.getX() + Constants.PADDLE_WIDTH < Constants.BOARD_LENGTH) {
-					commandClass.setCommand((paddleRightMoveCommand));
-					commandClass.buttonWasPressed();
-				}
-				break;
+					if (paddle.getX() + Constants.PADDLE_WIDTH < Constants.BOARD_LENGTH) {
+						commandClass.setCommand((paddleRightMoveCommand));
+						commandClass.buttonWasPressed();
+					}
+					break;
 			}
 		}
 		public void keyReleased(KeyEvent e) {}
